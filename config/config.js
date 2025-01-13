ec2ddac45d3cd0ddadf77240dc0635facc21f589
balenderk05 <balender.math2000@gmail.com>
2025-01-13 19:30:49 +0530

    Update and rename config.json to config.js
    update config file

---
+++ config/config.js
@@ -0,0 +1,33 @@
+// config/config.js
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "mysql"
+  }
+};
